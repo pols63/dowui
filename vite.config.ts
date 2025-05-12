cccd78d646db57d336c11994936edbc87ad28162
jean.sanchez@grupotawa.com <jean.sanchez@grupotawa.com>
2025-05-12 15:47:13 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
 		vue(),
 		vueDevTools(),
+		dts({
+			include: ['src'],
+			insertTypesEntry: true,
+			outDir: 'dist',
+		})
 	],
 	resolve: {
 		alias: {
