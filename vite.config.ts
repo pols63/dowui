ac94700194235fa7eaffff565e8ba95e1bc17a5f
jean.sanchez@grupotawa.com <jean.sanchez@grupotawa.com>
2025-05-12 15:02:50 -0500

    dev

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,20 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+	plugins: [
+		vue(),
+		vueDevTools(),
+	],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		},
+	},
+	build: {
+		lib: {
+			entry: 'src/index.ts', // 👈 Le dices a Vite dónde está tu archivo de entrada
+			name: 'Dowui',
+			fileName: (format) => `index.${format}.js`,
+		}
+	}
 })
